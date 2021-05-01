9d6294dae74a6a56c344b83c5f32149bb24a7d48
Olek Myron <myrondell@gmail.com>
2021-05-01 18:17:40 +0300

    ~

--- js/js-test.js
+++ js/js-test.js
@@ -1,6 +1,9 @@
 // // ===== Почему ...
 // function getProductPrice(productName) {
 //   let target = null;  //------так работает
+
+// const { format } = require("prettier");
+
   
 //   for (let i = 0; i < products.length; i += 1) {
 //     const obj = products[i];
@@ -122,16 +125,138 @@ const vehicles = [
 
 // -------- Полифил Object.entries()
 
-const objChange = obj => {
-    const objNew = [];
-    const keys = Object.keys(obj);
-    const values = Object.values(obj);
+// const objChange = obj => {
+//     const objNew = [];
+//     const keys = Object.keys(obj);
+//     const values = Object.values(obj);
 
-    for (let i = 0; i < keys.length; i += 1){
-        const buff = [];
-        objNew.push([keys[i]])
-        objNew[i].push(values[i])
-    }
+//     for (let i = 0; i < keys.length; i += 1){
+//         const buff = [];
+//         objNew.push([keys[i]])
+//         objNew[i].push(values[i])
+//     }
 
-    return objNew;
-}
+//     return objNew;
+// }
+
+
+// ------- Формат номера телефона
+
+// const foo = array => array.reduce((format, elem) => format.replace('x', elem), '(xxx) xxx-xxxx')
+// console.log(foo([0,9,7,6,5,4,7,8,3,3]));
+
+
+
+const players = [
+  { name: 'Манго', playtime: 1270, gamesPlayed: 4 },
+  { name: 'Поли', playtime: 469, gamesPlayed: 2 },
+  { name: 'Аякс', playtime: 690, gamesPlayed: 3 },
+  { name: 'Киви', playtime: 241, gamesPlayed: 1 },
+];
+
+
+// ------- Изменяем одно свойство в массиве объектов с помощью Object.map()
+
+// const updatedPlayer = 'Аякс';
+
+// const updateValue = (array, playerName) => {
+//     let updArray = [];
+//     array.map(el => {
+//         if (el.name === playerName) {
+//             el = {
+//                 ...el,
+//                 gamesPlayed: el.gamesPlayed + 1,
+//             }
+//         }
+//         updArray.push(el);
+//     })
+//     return updArray;
+// }
+// console.table(updateValue(players, updatedPlayer));
+
+
+// --------sum of 2 smallest nums in array
+
+const users = [
+  {
+    name: 'Moore Hensley',
+    email: 'moorehensley@indexia.com',
+    eyeColor: 'blue',
+    friends: ['Sharron Pace'],
+    isActive: false,
+    balance: 2811,
+    gender: 'male',
+    age: 37
+  },
+  {
+    name: 'Sharlene Bush',
+    email: 'sharlenebush@tubesys.com',
+    eyeColor: 'blue',
+    friends: ['Briana Decker', 'Sharron Pace'],
+    isActive: true,
+    balance: 3821,
+    gender: 'female',
+    age: 34
+  },
+  {
+    name: 'Ross Vazquez',
+    email: 'rossvazquez@xinware.com',
+    eyeColor: 'green',
+    friends: ['Marilyn Mcintosh', 'Padilla Garrison', 'Naomi Buckner'],
+    isActive: false,
+    balance: 3793,
+    gender: 'male',
+    age: 24
+  },
+  {
+    name: 'Elma Head',
+    email: 'elmahead@omatom.com',
+    eyeColor: 'green',
+    friends: ['Goldie Gentry', 'Aisha Tran'],
+    isActive: true,
+    balance: 2278,
+    gender: 'female',
+    age: 21
+  },
+  {
+    name: 'Carey Barr',
+    email: 'careybarr@nurali.com',
+    eyeColor: 'blue',
+    friends: ['Jordan Sampson', 'Eddie Strong'],
+    isActive: true,
+    balance: 3951,
+    gender: 'male',
+    age: 27
+  },
+  {
+    name: 'Blackburn Dotson',
+    email: 'blackburndotson@furnigeer.com',
+    eyeColor: 'brown',
+    friends: ['Jacklyn Lucas', 'Linda Chapman'],
+    isActive: false,
+    balance: 1498,
+    gender: 'male',
+    age: 38
+  },
+  {
+    name: 'Sheree Anthony',
+    email: 'shereeanthony@kog.com',
+    eyeColor: 'brown',
+    friends: ['Goldie Gentry', 'Briana Decker'],
+    isActive: true,
+    balance: 2764,
+    gender: 'female',
+    age: 39
+  }
+]
+
+
+// --------- Вычисление количества друзей
+
+// const foo = array => array.reduce((acc, el) => [...acc, ...el.friends], []).reduce((acc, el) => {
+//   return {
+//     ...acc,
+//     [el]: acc[el] ? acc[el] + 1 : 1,
+//   }
+// },{});
+// console.log(foo(users))
